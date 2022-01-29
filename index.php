53b9b984f7a9ec0507bbd085337563740fe70647
LauraSantos <lala123nielsen@gmail.com>
2022-01-29 15:39:33 -0300

    teste

--- index.php
+++ index.php
@@ -3,14 +3,37 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
+    <title>CCF - Alterar senha</title>
+    <link rel="shortcut icon" href="chutando_bola.png">
+    <link rel="stylesheet" href="estilos.css">
+
+    <script language="Javascript">
+        function mensagem(){
+            alert("Cheque seu email e altere sua senha, após isso tente efetuar o login novamente!!!");
+        }
+    </script>
 </head>
-<body>
-    <h1>Teste<h1>
+
+<body id="body1">
+    <div class="container">
+        <div class="quadrado"></div>
+        <div class="field">
+            <input type="email" id="email" placeholder="Confirme seu email">
+            <label for="email">Confirme seu email:</label>
+        </div>
+
+        <div class="center">
+            <a href="http://127.0.0.1:5500/trabalho_ferias/login.html">
+            <button class="button2" onclick="mensagem()">Continuar</button></a>
+        </div>
+
+        <div class="links2">
+            <a href="http://127.0.0.1:5500/trabalho_ferias/login.html"><h2>Cancelar</h2></a>
+        </div>
+    </div>
 </body>
 </html>
